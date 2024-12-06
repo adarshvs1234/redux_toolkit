b5150342267a4fed105159a6e762b2785484cdb6
adarshvs1234 <va854173@gmail.com>
2024-12-06 09:48:06 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-const CAKE
+const redux = require('redux')
+
+const createStore = redux.legacy_createStore
+const CAKE_ORDERED = 'CAKE_ORDERED'
+
+
+function orderCake(){
+
+    return{
+    type : CAKE_ORDERED,
+    quantity:1
+}
+
+}
+
+
+const initialState={
+numOfCakes:10,
+
+}
+
+
+const reducer = (state=initialState,action)=>{
+
+    switch(action.type){
+
+        case CAKE_ORDERED:
+            return{
+                ...state,
+                numOfCakes : state.numOfCakes-1
+            }
+
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer)
+console.log('initial state',initialState);
+
+
+
+const unsubscribe = store.subscribe(()=>console.log(store.getState()))
+
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+unsubscribe()
---
+++ store1.js
@@ -0,0 +1,24 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+createAsyncThunk('api/data',()=>{
+
+    return axios.get('http://localhost:8090/data')
+        .then((response)=>{
+
+            return response.data
+        })
+})
+
+
+
+
+const dataInfoSice = createSlice({
+
+    name:'data',
+    initialState:0,
+    dataI
+}
+)
+
+export default store
+
+
